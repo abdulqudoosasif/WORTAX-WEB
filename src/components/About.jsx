a9dfb2507eef84b9c170ed452331d0afd50078ed
Abdul Qudoos <abdul.qud00s.se@gmail.com>
2025-01-01 21:24:20 +0500

    landing page. fixed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function About() {
   return (
-    <div data-scroll data-scroll-section data-scroll-speed='0.2' className='bg-gray-400 rounded-t-3xl -mt-[4vw] h-screen  relative z-20'>
+    <div data-scroll data-scroll-section data-scroll-speed='0.2' className='bg-gray-400 rounded-t-3xl -mt-[15vw] h-screen  relative z-20'>
       
     </div>
   )
--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 function Marquee() {
 
   return (
-    <div data-scroll data-scroll-section data-scroll-speed='-.3' className='w-full py-20 pb-32 rounded-t-3xl bg-neutral-900 mt-16 relative z-20'>
+    <div data-scroll data-scroll-section  className='w-full py-20 pb-32 rounded-t-3xl bg-neutral-900 mt-16 relative z-20'>
       <div className=' border-y-[1px]  border-zinc-300 flex  overflow-hidden whitespace-nowrap'>
  <motion.h1 initial={{x:0}} animate={{x:'-100%'}} transition={{repeat:Infinity, ease:"linear",duration:15}}  className=' text-[20vw] leading-none text-white font-bold '> WE ARE WORTAX • </motion.h1>
  <motion.h1 initial={{x:0}} animate={{x:'-100%'}} transition={{repeat:Infinity, ease:"linear",duration:15}} className=' text-[20vw] leading-none text-white font-bold '>   WE ARE WORTAX •</motion.h1>
