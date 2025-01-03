fd25abba3c650e802bf95413c569ecdf11fdf130
hAmzA-491 <services.hm01@gmail.com>
2025-01-03 13:00:28 +0500

    padding

--- src/components/home/services/Services.jsx
+++ src/components/home/services/Services.jsx
@@ -5,8 +5,8 @@ const WalletHoverEffect = () => {
   return (
     <div className="flex flex-col py-10 min-h-screen bg-gray-50">
       <ServicesHeader />
-      <div className="flex items-center justify-evenly flex-wrap">
-        <div className="relative lg:w-[calc(50%-50px)] md:w-[calc(100%-50px)] w-[calc(100%-40px)] my-10 h-48 group hover:skew-x-3 perspective-1000">
+      <div className="flex items-center justify-evenly flex-wrap px-8">
+        <div className="relative lg:w-[calc(50%-50px)] md:w-[calc(100%-50px)] w-[calc(100%-40px)] my-10 h-48 group hover:skew-x-3 perspective-1000 scale-y-110">
           {/* {/ Card wrapper for hover effect /} */}
           <div className="relative w-full h-full transform transition-transform duration-700 transform-style-preserve-3d group-hover:rotate-x-40 group-hover:translate-z-20">
             {/* {/ Colored layers /} */}
@@ -38,7 +38,7 @@ const WalletHoverEffect = () => {
             </div>
           </div>
         </div>
-        <div className="relative lg:w-[calc(50%-50px)] md:w-[calc(100%-50px)] w-[calc(100%-40px)] my-10 h-48 group hover:skew-x-3 perspective-1000">
+        <div className="relative lg:w-[calc(50%-50px)] md:w-[calc(100%-50px)] w-[calc(100%-40px)] my-10 h-48 group hover:skew-x-3 perspective-1000 scale-y-110">
           {/* {/ Card wrapper for hover effect /} */}
           <div className="relative w-full h-full transform transition-transform duration-700 transform-style-preserve-3d group-hover:rotate-x-40 group-hover:translate-z-20">
             {/* {/ Colored layers /} */}
@@ -70,7 +70,7 @@ const WalletHoverEffect = () => {
             </div>
           </div>
         </div>
-        <div className="relative lg:w-[calc(50%-50px)] md:w-[calc(100%-50px)] w-[calc(100%-40px)] my-10 h-48 group hover:skew-x-3 perspective-1000">
+        <div className="relative lg:w-[calc(50%-50px)] md:w-[calc(100%-50px)] w-[calc(100%-40px)] my-10 h-48 group hover:skew-x-3 perspective-1000 scale-y-110">
           {/* {/ Card wrapper for hover effect /} */}
           <div className="relative w-full h-full transform transition-transform duration-700 transform-style-preserve-3d group-hover:rotate-x-40 group-hover:translate-z-20">
             {/* {/ Colored layers /} */}
@@ -102,7 +102,7 @@ const WalletHoverEffect = () => {
             </div>
           </div>
         </div>
-        <div className="relative lg:w-[calc(50%-50px)] md:w-[calc(100%-50px)] w-[calc(100%-40px)] my-10 h-48 group hover:skew-x-3 perspective-1000">
+        <div className="relative lg:w-[calc(50%-50px)] md:w-[calc(100%-50px)] w-[calc(100%-40px)] my-10 h-48 group hover:skew-x-3 perspective-1000 scale-y-110">
           {/* {/ Card wrapper for hover effect /} */}
           <div className="relative w-full h-full transform transition-transform duration-700 transform-style-preserve-3d group-hover:rotate-x-40 group-hover:translate-z-20">
             {/* {/ Colored layers /} */}
@@ -134,7 +134,7 @@ const WalletHoverEffect = () => {
             </div>
           </div>
         </div>
-        <div className="relative lg:w-[calc(50%-50px)] md:w-[calc(100%-50px)] w-[calc(100%-40px)] my-10 h-48 group hover:skew-x-3 perspective-1000">
+        <div className="relative lg:w-[calc(50%-50px)] md:w-[calc(100%-50px)] w-[calc(100%-40px)] my-10 h-48 group hover:skew-x-3 perspective-1000 scale-y-110">
           {/* {/ Card wrapper for hover effect /} */}
           <div className="relative w-full h-full transform transition-transform duration-700 transform-style-preserve-3d group-hover:rotate-x-40 group-hover:translate-z-20">
             {/* {/ Colored layers /} */}
@@ -166,71 +166,7 @@ const WalletHoverEffect = () => {
             </div>
           </div>
         </div>
-        <div className="relative lg:w-[calc(50%-50px)] md:w-[calc(100%-50px)] w-[calc(100%-40px)] my-10 h-48 group hover:skew-x-3 perspective-1000">
-          {/* {/ Card wrapper for hover effect /} */}
-          <div className="relative w-full h-full transform transition-transform duration-700 transform-style-preserve-3d group-hover:rotate-x-40 group-hover:translate-z-20">
-            {/* {/ Colored layers /} */}
-            <div className="absolute inset-0 mb-1 bg-yellow-400 rounded-lg transform transition-transform duration-300 group-hover:-translate-y-8 z-0"></div>
-            <div className="absolute inset-0 mb-1 bg-purple-400 rounded-lg transform transition-transform duration-300 group-hover:-translate-y-6 z-0"></div>
-            <div className="absolute inset-0 mb-1 bg-teal-200 rounded-lg transform transition-transform duration-300 group-hover:-translate-y-4 z-0"></div>
-            <div className="absolute inset-0 mb-1 bg-pink-800 rounded-lg transform transition-transform duration-300 group-hover:-translate-y-2 z-0"></div>
-
-            {/* {/ Main card /} */}
-            <div className="relative bg-white rounded-lg shadow-lg p-6 flex flex-col items-center justify-center z-10 border border-gray-300">
-              <h2 className="text-lg font-semibold mb-4">Mobile Design</h2>
-              <div>
-                {/* {/ Mobile icon /} */}
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={2}
-                  stroke="currentColor"
-                  className="w-28 h-28 text-gray-700"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M15.75 9v.008h.008V9h-.008zM4.5 8.25v7.5a1.5 1.5 0 001.5 1.5h12a1.5 1.5 0 001.5-1.5v-7.5m-15 0A1.5 1.5 0 016 6.75h12a1.5 1.5 0 011.5 1.5m-15 0h15M9 12h6"
-                  />
-                </svg>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="relative lg:w-[calc(50%-50px)] md:w-[calc(100%-50px)] w-[calc(100%-40px)] my-10 h-48 group hover:skew-x-3 perspective-1000">
-          {/* {/ Card wrapper for hover effect /} */}
-          <div className="relative w-full h-full transform transition-transform duration-700 transform-style-preserve-3d group-hover:rotate-x-40 group-hover:translate-z-20">
-            {/* {/ Colored layers /} */}
-            <div className="absolute inset-0 mb-1 bg-yellow-400 rounded-lg transform transition-transform duration-300 group-hover:-translate-y-8 z-0"></div>
-            <div className="absolute inset-0 mb-1 bg-purple-400 rounded-lg transform transition-transform duration-300 group-hover:-translate-y-6 z-0"></div>
-            <div className="absolute inset-0 mb-1 bg-teal-200 rounded-lg transform transition-transform duration-300 group-hover:-translate-y-4 z-0"></div>
-            <div className="absolute inset-0 mb-1 bg-pink-800 rounded-lg transform transition-transform duration-300 group-hover:-translate-y-2 z-0"></div>
-
-            {/* {/ Main card /} */}
-            <div className="relative bg-white rounded-lg shadow-lg p-6 flex flex-col items-center justify-center z-10 border border-gray-300">
-              <h2 className="text-lg font-semibold mb-4">Mobile Design</h2>
-              <div>
-                {/* {/ Mobile icon /} */}
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={2}
-                  stroke="currentColor"
-                  className="w-28 h-28 text-gray-700"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M15.75 9v.008h.008V9h-.008zM4.5 8.25v7.5a1.5 1.5 0 001.5 1.5h12a1.5 1.5 0 001.5-1.5v-7.5m-15 0A1.5 1.5 0 016 6.75h12a1.5 1.5 0 011.5 1.5m-15 0h15M9 12h6"
-                  />
-                </svg>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="relative lg:w-[calc(50%-50px)] md:w-[calc(100%-50px)] w-[calc(100%-40px)] my-10 h-48 group hover:skew-x-3 perspective-1000">
+        <div className="relative lg:w-[calc(50%-50px)] md:w-[calc(100%-50px)] w-[calc(100%-40px)] my-10 h-48 group hover:skew-x-3 perspective-1000 scale-y-110">
           {/* {/ Card wrapper for hover effect /} */}
           <div className="relative w-full h-full transform transition-transform duration-700 transform-style-preserve-3d group-hover:rotate-x-40 group-hover:translate-z-20">
             {/* {/ Colored layers /} */}
--- src/components/home/services/ServicesHeader.jsx
+++ src/components/home/services/ServicesHeader.jsx
@@ -3,7 +3,7 @@ import text_overlay from '../../../assets/img/text-overlay.png'
 
 const ServicesHeader = () => {
   return (
-    <div className='flex lg:flex-row md:flex-col   lg:justify-between md:justify-start justify-start lg:items-center gap-3 flex-wrap  lg:px-10 md:px-7 px-5'>
+    <div className='flex lg:flex-row md:flex-col   lg:justify-between md:justify-start justify-start lg:items-center gap-3 flex-wrap  lg:px-16 md:px-7 px-5'>
       <h2 className='flex flex-col font-semibold lg:text-5xl md:text-4xl text-4xl'><span >Our</span> <img src={text_overlay} className='lg:w-[190px] md:w-[160px] w-40 lg:h-[70px] md:h-14 h-14 lg:ml-8 md:ml-2 ml-2' /><span className='absolute lg:mt-14 md:mt-12 mt-12 lg:ml-10 md:ml-[22px] ml-5  text-[#7B7B7B]'>  services</span></h2>
       <p className='lg:max-w-[400px]  lg:text-base md:text-base text-sm'>Assemble a fully capable design and development team overnight so you can scale fast 🙌</p>
     </div>
