401d2c4f22a4baf8d0863d37779e435d9f5ea793
Abdul Qudoos <abdul.qud00s.se@gmail.com>
2025-01-08 15:33:33 +0500

    three d

--- src/components/home/Marquee.jsx
+++ src/components/home/Marquee.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 function Marquee() {
 
   return (
-    <div data-scroll data-scroll-section  className='w-full py-12 lg:py-20 pb-28 md:mb-[40px] lg:mb-0 rounded-t-3xl bg-green-800 lg:mt-16 mt-12 relative z-20'>
+    <div data-scroll data-scroll-section  className='w-full py-12 lg:py-20 pb-28 md:mb-[40px] lg:mb-0 rounded-t-3xl bg-green-500 lg:mt-16 mt-12 relative z-20'>
       <div className=' border-y-[1px]  border-zinc-300 flex  overflow-hidden whitespace-nowrap'>
  <motion.h1 initial={{x:0}} animate={{x:'-100%'}} transition={{repeat:Infinity, ease:"linear",duration:15}}  className=' text-[20vw] leading-none text-gray-50 font-bold '> WE ARE WORTAX • </motion.h1>
  <motion.h1 initial={{x:0}} animate={{x:'-100%'}} transition={{repeat:Infinity, ease:"linear",duration:15}} className=' text-[20vw] leading-none text-gray-50 font-bold '>   WE ARE WORTAX •</motion.h1>
