ab8f86fa63d19486815b6076bfab1cb41042f76d
Abdul Qudoos <abdul.qud00s.se@gmail.com>
2025-01-09 12:52:06 +0500

    about section fixed

--- src/components/home/3dLaptop/MainLaptopFile.jsx
+++ src/components/home/3dLaptop/MainLaptopFile.jsx
@@ -4,7 +4,7 @@ import ThreeDmodel from './ThreeDmodel'
 function MainLaptopFile() {
   return (
     <div className='h-[100vh] w-[100vw] items-center justify-center  rounded-b-3xl overflow-hidden lg:flex md:flex hidden z-20 bg-gray-50 relative'>
-      <h1 className='text-[13vw] text-nowrap font-extrabold'>GET STARTED</h1>
+      <h1 className='text-[13vw] text-nowrap font-bold'>GET STARTED</h1>
       <div className=' absolute w-full h-full z-10'>
         <ThreeDmodel/>
       </div>
