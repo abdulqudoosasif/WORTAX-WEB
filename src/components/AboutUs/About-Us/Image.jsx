9e7f0a07fe800f484717b8bcb8c836280e68cf9f
hAmzA-491 <services.hm01@gmail.com>
2025-01-14 11:20:21 +0500

    Image

--- src/components/AboutUs/About-Us/Image.jsx
+++ src/components/AboutUs/About-Us/Image.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
-
 const Image = () => {
     return (
-        <div className='w-full h-[80vh] rounded-2xl overflow-hidden bg-[#f1f1f1] mb-20'>
-            <img src="https://ochi.design/wp-content/uploads/2022/05/017091720030.jpg" alt="" className='h-[100vh] object-cover '
+        <div className='w-full h-[100vh] rounded-2xl overflow-hidden bg-[#f1f1f1] mb-20'>
+            <img src="https://ochi.design/wp-content/uploads/2022/05/017091720030.jpg" alt="" className='h-[60vw] object-cover '
                 data-scroll
                 data-scroll-section
                 data-scroll-speed="-.2" />
--- src/components/work/Tabs/TheProject.jsx
+++
@@ -1,8 +0,0 @@
-export function TheProject(props) {
-    return (
-        <div className="lg:w-[30%]  block  rounded-2xl overflow-hidden my-1">
-            
-            <img className='h-[100%] w-[100%] rounded-sm object-cover object-center' src={props.ProjectImgSrc} alt="Content Not Found" />
-        </div>
-    )
-}
