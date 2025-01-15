58abca1380d8594be03ac0ecb24bc609989986cf
hAmzA-491 <services.hm01@gmail.com>
2025-01-15 17:17:31 +0500

    About us responsive fixed

--- src/components/AboutUs/About-Us/Image.jsx
+++ src/components/AboutUs/About-Us/Image.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 const Image = () => {
     return (
-        <div className='w-full lg:h-[100vh] h-96 rounded-2xl overflow-hidden bg-[#f1f1f1] mb-20'>
-            <img src="https://ochi.design/wp-content/uploads/2022/05/017091720030.jpg" alt="" className='w-full lg:h-[60vw] h-96 object-cover rounded-2xl'
+        <div className='w-full lg:h-[100vh] h-96 rounded-2xl overflow-hidden bg-[#f1f1f1] lg:mb-20 mb-[88px]'>
+            <img src="https://ochi.design/wp-content/uploads/2022/05/017091720030.jpg" alt="" className='w-full lg:h-[60vw] h-80 object-cover rounded-2xl'
                 data-scroll
                 data-scroll-section
                 data-scroll-speed="-.2" />
