e383587d1a9eaf18b8dd9bb8be66d9ab70062eb0
Abdul Qudoos <abdul.qud00s.se@gmail.com>
2025-01-16 16:09:15 +0500

    change

--- src/components/AboutUs/Landing-Page/HeroSection.jsx
+++ src/components/AboutUs/Landing-Page/HeroSection.jsx
@@ -5,28 +5,31 @@ import wortax from "../../../assets/img/logo1.png";
 const HeroSection = () => {
 
   return (
-    <div className='absolute w-full h-screen bg-[#F1F1F1] lg:py-1 py-4'>
-      <div className='text-structure mt-[11.5vw] px-[5vw]'>
-        {["WE ARE", "WORTAX IT", "SOLUTIONS"].map((item, index) => {
-          return <div className='masker'>
-            <div className='w-fit flex items-center'>
-              {index === 1 && (
-                <motion.div initial={{ width: 0 }}
-                  animate={{ width: "10vw" }}
-                  transition={{ ease: [0.76, 0, 0.24, 1], duration: 1, delay:2.5 }}
-                  className=" lg:h-[6vw] md:h-[6vw] h-[8.3vw] overflow-hidden relative rounded-md  lg:top-[0.3vw] lg:ml-[0.3vw] md:-top-[0.1vw] sm:top-0 top-[1.2vw] mr-2 origin-left">
-                  <img src={wortax} 
-                   className="w-full lg:h-[6vw] md:h-[6vw] h-[8.3vw] object-cover rounded-lg shadow-lg"
-                  />
-                </motion.div>
-              )}
-              <h1 className="text-4xl uppercase lg:text-[7vw] font-bold lg:leading-[6vw] tracking-tighter  text-neutral-900">
-                {item}
-              </h1>
-            </div>
+    <div className='absolute w-full h-screen bg-[#F1F1F1] px-[5vw] lg:py-1 py-4'>
+      <div className="mt-[150px]  lg:mt-[16vw] ">
+            {["We are", "Wortax IT", "Solutions"].map((item, index) => {
+              return (
+                <div className="masker flex">
+                  {index === 1 && (
+                   <motion.div
+                   initial={{ width: 0 }}
+                   animate={{ width: '10vw' }}
+                   transition={{ ease: [0.76, 0, 0.24, 1], duration: 1, delay:2.5 }}
+                   className=" lg:h-[6vw] md:h-[6vw] h-[8.3vw] overflow-hidden relative rounded-md  lg:top-[0.3vw] lg:ml-[0.3vw] md:-top-[0.1vw] sm:top-0 top-[1.2vw] mr-2 origin-left"
+                 >
+                      <img
+                        src={wortax}
+                        className="w-full lg:h-[6vw] md:h-[6vw] h-[8.3vw] object-cover rounded-lg shadow-lg"
+                      />
+                    </motion.div>
+                  )}
+                  <h1 className="text-4xl uppercase lg:text-[7vw] font-bold lg:leading-[6vw] tracking-tighter  text-neutral-900">
+                    {item}
+                  </h1>
+                </div>
+              );
+            })}
           </div>
-        })}
-      </div>
     </div>
   )
 }
--- src/components/home/ParallaxDemo.jsx
+++ src/components/home/ParallaxDemo.jsx
@@ -1,49 +1,78 @@
 import React from 'react';
 import { ParallaxProvider, Parallax } from 'react-scroll-parallax';
-import Img1 from './../../assets/img/1.png'
+import Img1 from './../../assets/img/Review/1.png'
+import Img2 from './../../assets/img/Review/2.png'
+import Img3 from './../../assets/img/Review/3.png'
+import Img4 from './../../assets/img/Review/4.png'
+import Img5 from './../../assets/img/Review/5.png'
+import Img6 from './../../assets/img/Review/6.png'
+import Img7 from './../../assets/img/Review/7.png'
+import Img8 from './../../assets/img/Review/8.png'
+import Img9 from './../../assets/img/Review/15.png'
+import Img10 from './../../assets/img/Review/10.png'
+import Img11 from './../../assets/img/Review/11.png'
+import Img12 from './../../assets/img/Review/12.png'
+import Img13 from './../../assets/img/Review/13.png'
+import Img14 from './../../assets/img/Review/14.png'
 const ParallaxDemo = () => {
   return (
 <div className='lg:flex bg-white justify-between lg:h-[100vh] px-4 lg:px-16'> 
 
-    <div className='lg:w-[30vw] flex items-center'>
-      <div>
-      <h1 className='lg:text-[3.6vw] text-3xl font-semibold lg:leading-[3vw]'>Results that<br/>
+    <div className='lg:w-[30vw] md:w-full w-full flex items-center lg:justify-start md:justify-center justify-center'>
+      <div className='lg:py-0 md:py-10 py-6'>
+      <h1 className='lg:text-[3.6vw] text-3xl font-semibold lg:leading-[3vw] '>Results that<br className='lg:block md:hidden hidden'/>
         speak for</h1>
-        <h1 className='lg:text-[3.6vw] lg:mb-0  mb-4 text-2xl font-semibold leading-tight text-gray-500'>themselves</h1>
+        <h1 className='lg:text-[3.6vw] lg:mb-0  mb-4 text-2xl font-semibold leading-tight text-gray-500 text-center'>themselves</h1>
       </div>
     </div>
 <ParallaxProvider>
       <div className='flex overflow-hidden  gap-3  '>
         {/* Parallax Content */}
         <Parallax speed={-30} className='flex flex-col gap-4'>
-          <div className='lg:w-[25vw] rounded-lg h-[30vw] bg-slate-900 ] text-white flex items-center justify-center'>
-          <img src={Img1} alt="" />
+          <div className='lg:w-[29vw] rounded-lg   text-white flex items-center justify-center'>
+          <img src={Img1} alt="" className=' shadow-lg border '  />
           </div>
-          <div className='lg:w-[25vw] rounded-lg h-[20vw] bg-slate-900 ] text-white flex items-center justify-center'>
-           <img src={Img1} alt="" className='w-full' />
+          <div className='lg:w-[29vw] rounded-lg   text-white flex items-center justify-center'>
+           <img src={Img2} alt="" className=' shadow-lg border w-full' />
           </div>
-          <div className='lg:w-[25vw] rounded-lg h-[30vw] bg-slate-900 ] text-white flex items-center justify-center'>
-           <img src={Img1} alt="" className='w-full' />
+          <div className='lg:w-[29vw] rounded-lg   text-white flex items-center justify-center'>
+           <img src={Img3} alt="" className=' shadow-lg border w-full' />
           </div>
-          <div className='lg:w-[25vw] rounded-lg h-[20vw] bg-slate-900 ] text-white flex items-center justify-center'>
-           <img src={Img1} alt="" className='w-full' />
+          <div className='lg:w-[29vw] rounded-lg   text-white flex items-center justify-center'>
+           <img src={Img4} alt="" className=' shadow-lg border w-full' />
           </div>
-          
-      
+          <div className='lg:w-[29vw] rounded-lg   text-white flex items-center justify-center'>
+           <img src={Img5} alt="" className=' shadow-lg border w-full' />
+          </div>
+          <div className='lg:w-[29vw] rounded-lg   text-white flex items-center justify-center'>
+           <img src={Img10} alt="" className=' shadow-lg border w-full' />
+          </div>
+          <div className='lg:w-[29vw] rounded-lg   text-white flex items-center justify-center'>
+           <img src={Img11} alt="" className=' shadow-lg border w-full' />
+          </div>
         </Parallax>
 
         <Parallax speed={30} className='flex flex-col gap-4'>
-        <div className='lg:w-[25vw] rounded-lg h-[30vw] bg-slate-400  text-white flex items-center justify-center'>
-           <img src={Img1} alt="" className='w-full' />
+        <div className='lg:w-[29vw] rounded-lg  bg-slate-400  text-white flex items-center justify-center'>
+           <img src={Img6} alt="" className=' shadow-lg border w-full' />
           </div>
-          <div className='lg:w-[25vw] rounded-lg h-[20vw] bg-slate-400  text-white flex items-center justify-center'>
-           <img src={Img1} alt="" className='w-full' />
+          <div className='lg:w-[29vw] rounded-lg  bg-slate-400  text-white flex items-center justify-center'>
+           <img src={Img7} alt="" className=' shadow-lg border w-full' />
           </div>
-          <div className='lg:w-[25vw] rounded-lg h-[30vw] bg-slate-400  text-white flex items-center justify-center'>
-           <img src={Img1} alt="" className='w-full' />
+          <div className='lg:w-[29vw] rounded-lg  bg-slate-400  text-white flex items-center justify-center'>
+           <img src={Img8} alt="" className=' shadow-lg border w-full' />
           </div>
-          <div className='lg:w-[25vw] rounded-lg h-[20vw] bg-slate-400  text-white flex items-center justify-center'>
-           <img src={Img1} alt="" className='w-full' />
+          <div className='lg:w-[29vw] rounded-lg  bg-slate-400  text-white flex items-center justify-center'>
+           <img src={Img9} alt="" className=' shadow-lg border w-full' />
+          </div>
+          <div className='lg:w-[29vw] rounded-lg  bg-slate-400  text-white flex items-center justify-center'>
+           <img src={Img12} alt="" className=' shadow-lg border w-full' />
+          </div>
+          <div className='lg:w-[29vw] rounded-lg  bg-slate-400  text-white flex items-center justify-center'>
+           <img src={Img13} alt="" className=' shadow-lg border w-full' />
+          </div>
+          <div className='lg:w-[29vw] rounded-lg  bg-slate-400  text-white flex items-center justify-center'>
+           <img src={Img14} alt="" className=' shadow-lg border w-full' />
           </div>
         </Parallax>
       </div>
