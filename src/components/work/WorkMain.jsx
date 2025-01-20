37d7c99560ae2f49b9a780f07b821a4d12bf64aa
hAmzA-491 <services.hm01@gmail.com>
2025-01-20 18:17:05 +0500

    Update WorkMain.jsx

--- src/components/work/WorkMain.jsx
+++ src/components/work/WorkMain.jsx
@@ -1,51 +1,53 @@
+import { motion } from 'framer-motion';
 import React, { useState, useEffect } from 'react';
+import img from '../../assets/img/logo1.png'
 
 function WorkMain() {
   const [activeIndex, setActiveIndex] = useState(0);
 
-  const messages = [
-    "Your Unfair Advantage",
-    "Your Competitive Edge",
-    "Your Unfair Advantage",
-    "Your Competitive Edge",
-    "Your Unfair Advantage",
-    "Your Competitive Edge",
-  ];
+  // const messages = [
+  //   "Your Unfair Advantage",
+  //   "Your Competitive Edge",
+  //   "Your Unfair Advantage",
+  //   "Your Competitive Edge",
+  //   "Your Unfair Advantage",
+  //   "Your Competitive Edge",
+  // ];
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setActiveIndex((prevIndex) => (prevIndex + 1) % messages.length);
-    }, 2000); // Change every 2 seconds
-    return () => clearInterval(interval);
-  }, [messages.length]);
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setActiveIndex((prevIndex) => (prevIndex + 1) % messages.length);
+  //   }, 2000); // Change every 2 seconds
+  //   return () => clearInterval(interval);
+  // }, [messages.length]);
 
   return (
-    <div className='px-[5vw] relative z-20'>
-      <div className="lg:h-[60vh] md:[50vh] h-[40vh] flex items-center justify-center">
-        <div className="relative">
-          <h1 className="text-[7vw] lg:leading-[7vw]  font-semibold text-center text-neutral-900">
-            Designing Unparalleled <br /> Success Stories
-          </h1>
-          <div className="absolute bottom-[-4vw] flex flex-col font-semibold items-center right-5 bg-[#e069fe] h-[4vw] rounded-3xl w-[20vw] overflow-hidden -rotate-[7deg]">
-            <div
-              className="transition-transform absolute   duration-100 ease-in-out"
-              style={{ transform: `translateY(-${activeIndex * 4}vw)` }}
-            >
-              {messages.map((message, index) => (
-                <h2
-                  key={index}
-                  className=" text-[1.4vw] leading-[.3vw] text-center pt-[1.9vw] "
-                  style={{ height: "4vw" }}
-                >
-                  {message}
-                </h2>
-              ))}
-            </div>
+
+    <div className=" lg:pt-[16vw] lg:pb-[8vw] px-[5vw] ">
+      {["Designing", "Unparalleled ", "Success Stories"].map((item, index) => {
+        return (
+          <div className="masker flex">
+            {index === 1 && (
+              <motion.div
+                initial={{ width: 0 }}
+                animate={{ width: '10vw' }}
+                transition={{ ease: [0.76, 0, 0.24, 1], duration: 1, delay: 2.5 }}
+                className=" lg:h-[6vw] md:h-[6vw] h-[8.3vw] overflow-hidden relative rounded-md  lg:top-[0.vw] lg:ml-[0.3vw] md:-top-[0.1vw] sm:top-0 top-[1.2vw] mr-2 origin-left"
+              >
+                <img
+                  src={img}
+                  className="w-full lg:h-[6vw] md:h-[6vw] h-[8.3vw] object-cover rounded-lg shadow-lg"
+                />
+              </motion.div>
+            )}
+            <h1 className="text-4xl uppercase lg:text-[7vw] font-bold lg:leading-[6vw] tracking-tighter  text-neutral-900">
+              {item}
+            </h1>
           </div>
-        </div>
-      </div>
-      <div className='h-[1px] w-full bg-gray-600 mt-[2vw]'></div>
+        );
+      })}
     </div>
+
   );
 }
 
