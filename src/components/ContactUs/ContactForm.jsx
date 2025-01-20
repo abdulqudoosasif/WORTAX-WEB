cb766d76d1a3ae6b40aa1efdb639588adb06a58c
Abdul Qudoos <abdul.qud00s.se@gmail.com>
2025-01-20 16:40:41 +0500

    change

--- src/components/ContactUs/ContactForm.jsx
+++ src/components/ContactUs/ContactForm.jsx
@@ -6,7 +6,7 @@ const  ContactForm = () => {
       {/* Left Section */}
       <div className="md:w-1/2 lg:sticky lg:top-[7vw] space-y-4">
         <p className="uppercase text-[2vw]font-medium tracking-wide">Contact</p>
-        <h1 className="text-[3vw] font-semibold leading-tight">
+        <h1 className="lg:text-[3vw] font-semibold leading-tight">
           Hi there! What brings <span className="italic">you</span> here today?
         </h1>
         <p className="text-gray-600">
@@ -23,34 +23,35 @@ const  ContactForm = () => {
       {/* Right Section */}
       <div className="md:w-1/2 mt-8 md:mt-0">
         <div className="space-y-4">
-          <h2 className="text-[4vw] font-light">Hire Amp</h2>
-          <p className="text-neutral-900 font-light text-[2vw]">A few questions to get started</p>
+          <h2 className="lg:text-[4vw] text-4xl font-light">Hire Amp</h2>
+          <p className="text-neutral-900 font-light lg:text-[2vw]">A few questions to get started</p>
 
           {/* Form */}
           <form className="space-y-6">
             {/* Name Fields */}
-            <div className="flex mt-[2vw] gap-4">
+            <div className="lg:flex mt-[2vw] gap-4">
               <div className="flex-1">
-                <label className="block  text-[1.6vw] font-light" htmlFor="firstName">
+                <label className="block  lg:lg:text-[1.6vw] font-light" htmlFor="firstName">
                   Your Name*
                 </label>
                 <input
                   type="text"
                   id="firstName"
                   placeholder="First Name"
-                  className="mt-1 w-full border-b-2 bg-gray-50 border-gray-300 text-[1.2vw] focus:border-black outline-none"
+                  className="mt-1 w-[82vw] md:w-full lg:w-full border-b-2
+                   bg-gray-50 border-gray-300  lg:text-[1.2vw] focus:border-black outline-none"
                   required
                 />
               </div>
               <div className="flex-1">
-                <label className="block text-[1.6vw] font-light" htmlFor="lastName">
+                <label className="block lg:text-[1.6vw] font-light" htmlFor="lastName">
                   &nbsp;
                 </label>
                 <input
                   type="text"
                   id="lastName"
                   placeholder="Last Name"
-                  className="mt-1 w-full border-b-2 bg-gray-50 border-gray-300 text-[1.2vw] focus:border-black outline-none"
+                  className="mt-1 w-[82vw] md:w-full lg:w-full border-b-2 bg-gray-50 border-gray-300  lg:text-[1.2vw] focus:border-black outline-none"
                   required
                 />
               </div>
@@ -58,82 +59,82 @@ const  ContactForm = () => {
 
             {/* Email Field */}
             <div>
-              <label className="block text-[1.6vw] font-light" htmlFor="email">
+              <label className="block lg:text-[1.6vw] font-light" htmlFor="email">
                 Email*
               </label>
               <input
                 type="email"
                 id="email"
                 placeholder="Work Email"
-                className="mt-1 w-full border-b-2 bg-gray-50 border-gray-300 text-[1.2vw] focus:border-black outline-none"
+                className="mt-1 w-[82vw] md:w-full lg:w-full border-b-2 bg-gray-50 border-gray-300  lg:text-[1.2vw] focus:border-black outline-none"
                 required
               />
             </div>
 
             {/* Company Field */}
             <div>
-              <label className="block text-[1.6vw] font-light" htmlFor="company">
+              <label className="block lg:text-[1.6vw] font-light" htmlFor="company">
                 Company*
               </label>
               <input
                 type="text"
                 id="company"
                 placeholder="Company Name"
-                className="mt-1 w-full border-b-2 bg-gray-50 border-gray-300 text-[1.2vw] focus:border-black outline-none"
+                className="mt-1 w-[82vw] md:w-full lg:w-full border-b-2 bg-gray-50 border-gray-300  lg:text-[1.2vw] focus:border-black outline-none"
                 required
               />
             </div>
 
             {/* Title Field */}
             <div>
-              <label className="block text-[1.6vw] font-light" htmlFor="title">
+              <label className="block lg:text-[1.6vw] font-light" htmlFor="title">
                 Title*
               </label>
               <input
                 type="text"
                 id="title"
                 placeholder="Your Title"
-                className="mt-1 w-full border-b-2 bg-gray-50 border-gray-300 text-[1.2vw] focus:border-black outline-none"
+                className="mt-1 w-[82vw] md:w-full lg:w-full border-b-2 bg-gray-50 border-gray-300  lg:text-[1.2vw] focus:border-black outline-none"
                 required
               />
             </div>
             <div>
-              <label className="block text-[1.6vw] font-light" htmlFor="email">
+              <label className="block lg:text-[1.6vw] font-light" htmlFor="email">
                 Email*
               </label>
               <input
                 type="email"
                 id="email"
                 placeholder="Work Email"
-                className="mt-1 w-full border-b-2 bg-gray-50 border-gray-300 text-[1.2vw] focus:border-black outline-none"
+                className="mt-1 w-[82vw] md:w-full lg:w-full border-b-2 bg-gray-50 border-gray-300  lg:text-[1.2vw] focus:border-black outline-none"
                 required
               />
             </div>
 
             {/* Company Field */}
             <div>
-              <label className="block text-[1.6vw] font-light" htmlFor="company">
+              <label className="block lg:text-[1.6vw] font-light" htmlFor="company">
                 Company*
               </label>
               <input
                 type="text"
                 id="company"
                 placeholder="Company Name"
-                className="mt-1 w-full border-b-2 bg-gray-50 border-gray-300 text-[1.2vw] focus:border-black outline-none"
+                className="mt-1 w-[82vw] md:w-full lg:w-full border-b-2 bg-gray-50 border-gray-300  lg:text-[1.2vw] focus:border-black outline-none"
                 required
               />
             </div>
 
             {/* Title Field */}
             <div>
-              <label className="block text-[1.6vw] font-light" htmlFor="title">
+              <label className="block lg:text-[1.6vw] font-light" htmlFor="title">
                 Title*
               </label>
               <input
                 type="text"
                 id="title"
                 placeholder="Your Title"
-                className="mt-1 w-full border-b-2 bg-gray-50 border-gray-300 text-[1.2vw] focus:border-black outline-none"
+                className="mt-1 w-[82vw] md:w-full lg:w-full border-b-2 bg-gray-50 border-gray-300  lg:text-[1.2vw] focus:border-black outline-none"
                 required
               />
             </div>
