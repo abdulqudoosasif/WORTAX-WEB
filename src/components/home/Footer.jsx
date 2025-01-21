515274cd4caf8397f1b2414fdb60a8d6d2f71cf7
Abdul Qudoos <abdul.qud00s.se@gmail.com>
2025-01-21 14:57:49 +0500

    change

--- src/components/home/Footer.jsx
+++ src/components/home/Footer.jsx
@@ -6,11 +6,12 @@ const Footer = () => {
   return (
  <>
 
- <div className='lg:h-[100vh] h-[110vh]' ></div>
-    <footer 
+ <div className='lg:h-[100vh] h-[110vh] ' >
+ <footer 
 
-className=" w-full bg-gray-50 justify-center pt-[200px] fixed -bottom-3 left-0 -z-10
- text-neutral-900 pb-[20px] px-5 lg:px-[5vw] min-h-[100vh] ">
+className=" w-full bg-gray-50 justify-center 
+ fixed -bottom-3 left-0 -z-10
+ text-neutral-900 pb-[20px] px-5 lg:px-[5vw] ] h-[100vh] ">
   <div className="px-6">
     <div className="grid lg:grid-cols-3  md:grid-cols-2 grid-cols-1 gap-5">
       {/* Left Section */}
@@ -105,6 +106,8 @@ className=" w-full bg-gray-50 justify-center pt-[200px] fixed -bottom-3 left-0 -
     </div>
   </div>
 </footer>
+ </div>
+   
  </>
   );
 };
--- src/components/home/LandingPage/BookAppointment.jsx
+++ src/components/home/LandingPage/BookAppointment.jsx
@@ -3,23 +3,19 @@ import { PopupButton } from "react-calendly";
 
 const BookAppointment = () => {
   return (
-
-    <div className="flex lg:flex-row flex-col lg:justify-between lg:items-center mt-3 lg:mb-[3vw]  gap-5 ">
-
     <div className="lg:flex justify-between pb-[50px] items-center mt-3 lg:mb-[3vw] lg:mx-16 mx-4">
-
         {[
           "For public and private companies",
           "From the first pitch to IPO",
         ].map((item, index) => (
-          <p className=" lg:my-2 lg:font-normal font-light ">{item}</p>
+          <p className=" my-2">{item}</p>
         ))}
         <PopupButton
           url="https://calendly.com/emhamza-491"
           rootElement={document.getElementById("root")}
-          className="relative w-fit px-5  lg:py-2 py-0 overflow-hidden border border-zinc-800 rounded-full text-[#17191b] transition-all before:absolute before:top-0 before:left-0 before:right-0 before:z-0 before:h-full before:w-0 before:bg-black before:transition-all before:duration-500 hover:text-white hover:before:left-0 hover:before:w-full"
+          className="relative px-5 text-sm py-2 overflow-hidden border border-zinc-800 rounded-full text-[#17191b] transition-all before:absolute before:top-0 before:left-0 before:right-0 before:z-0 before:h-full before:w-0 before:bg-black before:transition-all before:duration-500 hover:text-white hover:before:left-0 hover:before:w-full"
           text={
-            <span className="relative lg:text-sm text-xs z-10 tracking-wide font-medium">
+            <span className="relative z-10 tracking-wide font-medium">
               BOOK AN APPOINTMENT
             </span>
           }
